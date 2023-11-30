89e933a4c00e2e4acd5833707a1cb5b05571802a
Fayoza <fayozatohirovagit config --global user.email>
2023-11-30 17:24:04 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+//
+// for
+//
+
+// while
+// let i = 1
+// while(i <= 50){
+//     console.log(i)
+//     i++
+// }
+
+// do while
+// let i = 1
+// do{
+//     console.log(i)
+//     i++
+// }
+// while(i <= 50)
+
+
+
+
+
+//2
+// let num = +prompt("son kiring")
+
+// for(let i = 0; i < num; i++){
+//     console.log(i)
+// }
+
+
+// let num = +prompt("son kiring")
+
+// let i = 1
+// while(i <= num){
+//     console.log(i)
+//     i++
+// }
+
+
+// let num = +prompt("son kiring")
+
+// let i = 1
+// do{
+//     console.log(i)
+//     i++
+// }
+// while(i <= num)
+
+
+
+
+// let num = +prompt("sonni kiriting")
+
+// if(num % 3 == 0){
+//     console.log("Fizz")
+// }
+// else if(num % 5 == 0){
+//     console.log("Buzz")
+// }
+// else if(num % 3 == 0 && num % 5 == 0){
+//     console.log("FizzBuzz")
+// }
